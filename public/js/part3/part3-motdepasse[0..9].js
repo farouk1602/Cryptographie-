1a696365a041221d4e0444d7b8a19fa46169187d
farouk1602 <127437984+farouk1602@users.noreply.github.com>
2023-10-21 16:17:17 +0100

    Add files via upload

--- public/js/part3/part3-motdepasse[0..9].js
+++ public/js/part3/part3-motdepasse[0..9].js
@@ -9,4 +9,37 @@ function mot_de_passe_09() {
 
 }
 
+
+function bruteForceAttackDigits(targetPassword) {
+  const possibleChars = '0123456789';
+  const startTime = Date.now();
+
+  function recurse(currentPassword) {
+      if (currentPassword === targetPassword) {
+          const endTime = Date.now();
+          const elapsedTime = (endTime - startTime) / 1000;
+
+          document.getElementById('foundPassword').textContent = currentPassword;
+          document.getElementById('elapsedTime').textContent = elapsedTime;
+          return;
+      }
+
+      for (const char of possibleChars) {
+          recurse(currentPassword + char);
+      }
+  }
+
+  recurse('');
+}
+
+function startBruteForceAttackDigits() {
+  const targetPassword = document.getElementById('passwordInput').value;
+
+  if (targetPassword === '') {
+      alert('Veuillez entrer un mot de passe cible.');
+      return;
+  }
+
+  bruteForceAttackDigits(targetPassword);
+}
   
--- public/js/part3/part3-motdepasse[01].js
+++ public/js/part3/part3-motdepasse[01].js
@@ -11,3 +11,43 @@ function mot_de_passe_01() {
 
 
 
+function bruteForceAttack(targetPassword, passwordLength) {
+  const possibleChars = ['0', '1'];
+  const startTime = Date.now();
+
+  function recurse(currentPassword) {
+      if (currentPassword.length === passwordLength) {
+          if (currentPassword === targetPassword) {
+              const endTime = Date.now();
+              const elapsedTime = (endTime - startTime) / 1000;
+
+              document.getElementById('foundPassword').textContent = currentPassword;
+              document.getElementById('elapsedTime').textContent = elapsedTime;
+          }
+          return;
+      }
+
+      for (const char of possibleChars) {
+          recurse(currentPassword + char);
+      }
+  }
+
+  recurse('');
+}
+
+function startBruteForceAttack() {
+  const targetPassword = document.getElementById('passwordInput').value;
+  const passwordLength = targetPassword.length;
+
+  if (passwordLength === 0) {
+      alert('Veuillez entrer un mot de passe cible.');
+      return;
+  }
+
+  bruteForceAttack(targetPassword, passwordLength);
+}
+
+
+
+
+
