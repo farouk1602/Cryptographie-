4be6a9760991bc20c873e6db9910797f44b365ef
farouk1602 <127437984+farouk1602@users.noreply.github.com>
2023-11-05 18:32:24 +0100

    Add files via upload

--- public/js/part3/bruteforce3.js
+++ public/js/part3/bruteforce3.js
@@ -1,36 +1,36 @@
-const char1 ="01";
+const char3 ="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+[]{}|;:',./<>?";
 
 
-function bruteForceAttack01(targetPassword) {
-  const maxIndex = char1.length;
+function bruteForceAttackAll(targetPassword) {
+  const maxIndex = char3.length;
   let currentPassword = "";
   const startTime = new Date();
 
   function generateNextPassword(password) {
     if (password === "") {
-      return char1[0];
+      return char3[0];
     }
 
     let i = password.length - 1;
     let carry = true;
 
     while (i >= 0 && carry) {
-      const charIndex = char1.indexOf(password[i]);
+      const charIndex = char3.indexOf(password[i]);
       if (charIndex < maxIndex - 1) {
         password =
           password.substring(0, i) +
-          char1[charIndex + 1] +
+          char3[charIndex + 1] +
           password.substring(i + 1);
         carry = false;
       } else {
         password =
-          password.substring(0, i) + char1[0] + password.substring(i + 1);
+          password.substring(0, i) + char3[0] + password.substring(i + 1);
         i--;
       }
     }
 
     if (carry) {
-      password = char1[0] + password;
+      password = char3[0] + password;
     }
 
     return password;
@@ -58,13 +58,13 @@ function bruteForceAttack01(targetPassword) {
   };
 }
 
-function startBruteForceAttack01() {
-  const targetPassword = document.getElementById("passwordInput1").value;
+function startBruteForceAttackAll() {
+  const targetPassword = document.getElementById("passwordInput3").value;
 
   if (targetPassword === "") {
     alert("Veuillez entrer un mot de passe.");
     return;
   }
 
-  bruteForceAttack01(targetPassword);
+  bruteForceAttackAll(targetPassword);
 }
