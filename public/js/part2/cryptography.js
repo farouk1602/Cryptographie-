96d406f8ebf9f27ade538d16e054eedfb86f287c
Mounir Yacine <97355898+Mouniryacine@users.noreply.github.com>
2023-11-07 00:42:29 +0100

    part4

--- public/js/part2/cryptography.js
+++ public/js/part2/cryptography.js
@@ -1,26 +1,29 @@
-// cesar
+// Cesar
 function chiffrementCesar(message, key) {
   let resultat = "";
-
-  for (let i = 0; i < message.length; i++) {
-    let caractere = message[i];
-    if (caractere.match(/[a-zA-Z]/)) {
-      const code = message.charCodeAt(i);
-      let lettreDecalee;
-      if (caractere.match(/[a-z]/)) {
-        // Encryption for lowercase letters
-        lettreDecalee = String.fromCharCode(((code - 97 + key) % 26) + 97);
+  if (key % 26 == 0 || key == 1 || key % 13 == 0) {
+    alert("Weak Key!!");
+  } else {
+    for (let i = 0; i < message.length; i++) {
+      let caractere = message[i];
+      if (caractere.match(/[a-zA-Z]/)) {
+        const code = message.charCodeAt(i);
+        let lettreDecalee;
+        if (caractere.match(/[a-z]/)) {
+          // Encryption for lowercase letters
+          lettreDecalee = String.fromCharCode(((code - 97 + key) % 26) + 97);
+        } else {
+          // Encryption for uppercase letters
+          lettreDecalee = String.fromCharCode(((code - 65 + key) % 26) + 65);
+        }
+        resultat += lettreDecalee;
       } else {
-        // Encryption for uppercase letters
-        lettreDecalee = String.fromCharCode(((code - 65 + key) % 26) + 65);
+        resultat += caractere;
       }
-      resultat += lettreDecalee;
-    } else {
-      resultat += caractere;
     }
-  }
 
-  return resultat;
+    return resultat;
+  }
 }
 
 function dechiffrementCesar(message, key) {
@@ -47,23 +50,41 @@ function dechiffrementCesar(message, key) {
   return resultat;
 }
 
-
-
-
-
 //decalage
-function cryptageDecalage(text) {
-  const words = text.split(" ");
-  console.log(words)
+function decalageDroit(text) {
+  msgdecaler = [];
+  words = text.split(" ");
   for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    const newword = word.split("") // Reverse each word
-    console.log(newword.length)
-    console.log(newword)
+    text = words[i];
+    wordDecaler = text;
+    let letter = text[text.length - 1];
+    wordDecaler = wordDecaler.split("");
+    wordDecaler.pop();
+    wordDecaler.unshift(letter);
+    wordDecaler = wordDecaler.join("");
+
+    msgdecaler[i] = wordDecaler;
   }
-  return words;
+  msgdecaler = msgdecaler.join(" ");
+  return msgdecaler;
 }
-cryptageDecalage("qbc dwe")
+function decalageGauche(text) {
+  msgdecaler = [];
+  words = text.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    text = words[i];
+    wordDecaler = text;
+    let letter = text[0];
+    wordDecaler = wordDecaler.split("");
+    wordDecaler.shift();
+    wordDecaler.push(letter);
+    wordDecaler = wordDecaler.join("");
+
+    msgdecaler[i] = wordDecaler;
+  }
+  msgdecaler = msgdecaler.join(" ");
+  return msgdecaler;
+}
 
 /* function decryptDecalage(text) {
   let decryptedMessage = "";
@@ -78,10 +99,6 @@ cryptageDecalage("qbc dwe")
   return decryptedMessage;
 }  */
 
-
-
-
-
 // affine
 function verifyPrime(keyA, modulo) {
   function gcd(keyA, modulo) {
@@ -95,22 +112,30 @@ function verifyPrime(keyA, modulo) {
   return gcd(modulo, keyA % modulo) === 1;
 }
 function cryptageAffine(text, keyA, keyB) {
-  let cryptedMessage = "";
-  let newLetter;
-  Array.from(text).forEach((letter) => {
-    const code = letter.charCodeAt(0);
-    if (letter.match(/[a-z]/)) {
-      // Encryption for lowercase letters
-      newLetter = String.fromCharCode((((code - 97) * keyA + keyB) % 26) + 97);
-    } else if (letter.match(/[A-Z]/)) {
-      // Encryption for uppercase letters
-      newLetter = String.fromCharCode((((code - 65) * keyA + keyB) % 26) + 65);
-    } else {
-      newLetter = String.fromCharCode(code);
-    }
-    cryptedMessage = cryptedMessage + newLetter;
-  });
-  return cryptedMessage;
+  if (keyA == 1|| keyA % 26 == 0) {
+    alert("weak key!!");
+  } else {
+    let cryptedMessage = "";
+    let newLetter;
+    Array.from(text).forEach((letter) => {
+      const code = letter.charCodeAt(0);
+      if (letter.match(/[a-z]/)) {
+        // Encryption for lowercase letters
+        newLetter = String.fromCharCode(
+          (((code - 97) * keyA + keyB) % 26) + 97
+        );
+      } else if (letter.match(/[A-Z]/)) {
+        // Encryption for uppercase letters
+        newLetter = String.fromCharCode(
+          (((code - 65) * keyA + keyB) % 26) + 65
+        );
+      } else {
+        newLetter = String.fromCharCode(code);
+      }
+      cryptedMessage = cryptedMessage + newLetter;
+    });
+    return cryptedMessage;
+  }
 }
 
 function decryptAffine(text, keyA, keyB) {
@@ -141,36 +166,26 @@ function decryptAffine(text, keyA, keyB) {
   return decryptedMessage;
 }
 
-
-
-
-
 //mirroir
 function Palindrome(word) {
   return word === word.split("").reverse().join("");
 }
-function Mirroir(phrase) {
-  const words = phrase.split(" "); // Split the phrase into words
+function Mirroir(phrase, key) {
+  const words = phrase.split(" ");
   const reversedWords = [];
 
   for (let i = 0; i < words.length; i++) {
-    const word = words[i];
+    let word = words[i];
     if (Palindrome(word)) {
-      console.log("palindrom word detected:" + word);
+      word = chiffrementCesar(word, key);
     }
-    const reversedWord = word.split("").reverse().join(""); // Reverse each word
+    const reversedWord = word.split("").reverse().join("");
     reversedWords.push(reversedWord);
   }
 
-  const reversedPhrase = reversedWords.join(" "); // Recreate the reversed phrase
-
+  const reversedPhrase = reversedWords.join(" ");
   return reversedPhrase;
 }
-
-
-
-
-
 
 //apply
 function applyEncryption() {
@@ -189,14 +204,15 @@ function applyEncryption() {
     let gcd = verifyPrime(key, 26);
     if (gcd) {
       result = cryptageAffine(message, key, keyB);
-      document.getElementById("errorText").innerHTML = "";
     } else {
-      document.getElementById("errorText").innerHTML = "A And Modulo Not Prime";
+      alert("A And Modulo Not Prime");
     }
-  } else if (selectedMethod === "Decalage") {
-    result = chiffrementCesar(message, 1);
+  } else if (selectedMethod === "DecalageDroit") {
+    result = decalageDroit(message);
+  } else if (selectedMethod === "DecalageGauche") {
+    result = decalageGauche(message);
   } else if (selectedMethod === "Mirroir") {
-    result = Mirroir(message);
+    result = Mirroir(message, 2);
   }
 
   document.getElementById("result").textContent = result;
@@ -215,18 +231,16 @@ function applyDecryption() {
     const key = parseInt(document.getElementById("keyA").value);
     const keyB = parseInt(document.getElementById("keyB").value);
     result = decryptAffine(message, key, keyB);
-  } else if (selectedMethod === "Decalage") {
-    result = dechiffrementCesar(message, 1);
+  } else if (selectedMethod === "DecalageDroit") {
+    result = decalageGauche(message);
+  } else if (selectedMethod === "DecalageGauche") {
+    result = decalageDroit(message);
   } else if (selectedMethod === "Mirroir") {
-    result = Mirroir(message);
-    // Call your Mirror decryption function here
+    result = Mirroir(message, -2);
   }
 
   document.getElementById("result").textContent = result;
 }
-
-
-
 
 //file
 document.getElementById("fileButton").addEventListener("click", function () {
@@ -249,23 +263,35 @@ document.getElementById("fileInput").addEventListener("change", function () {
   }
 });
 
-
-
-
-
 //download file
-document.getElementById("downloadButton").addEventListener("click", function () {
-  const textarea = document.getElementById("result").value;
-  const blob = new Blob([textarea], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
+document
+  .getElementById("downloadButton")
+  .addEventListener("click", function () {
+    const textarea = document.getElementById("result").value;
+    const blob = new Blob([textarea], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
 
-  const downloadLink = document.createElement("a");
-  downloadLink.href = url;
-  downloadLink.download = "downloaded_file.txt";
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "downloaded_file.txt";
 
-  // Trigger a click event on the download link to start the download
-  downloadLink.click();
+    // Trigger a click event on the download link to start the download
+    downloadLink.click();
 
-  // Clean up by revoking the object URL
-  URL.revokeObjectURL(url);
-});
+    // Clean up by revoking the object URL
+    URL.revokeObjectURL(url);
+  });
+
+
+const correctPassword = "j8$7Kl9oP2#dFgH@3lAsQ1rT5xY6^bNhGm9*pLo0iJ2nBvF8cV4k3#7e3%zX1&oR7qW1@uE4yR6tI9wS3^mCcD5fG8hJkL2nM4bV6c8xZ9@oP2kL3iN5mB7vF5gH2jK4lA6sD8fG0hJ9&j8$7Kl9oP2#dFgH@3lAsQ1rT5xY6^bNhGm9*pLo0iJ2nBvF8cV4k3#7e3%zX1&oR7qW1@uE4yR6tI9wS3^mCcD5fG8hJkL2nM4bV6c8xZ9@oP2kL3iN5mB7vF5gH2jK4lA6sD8fG0hJ9&"
+;
+
+function checkPassword() {
+    const enteredPassword = document.getElementById("passwordinput").value;
+    if (enteredPassword === correctPassword) {
+        document.getElementById("decrypt").removeAttribute("disabled");
+        document.getElementById("crypt").removeAttribute("disabled");
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
+}
--- public/js/part3/bruteforce2.js
+++ public/js/part3/bruteforce2.js
@@ -1,36 +1,36 @@
-const char1 ="01";
+const char2 ="0123456789";
 
 
-function bruteForceAttack01(targetPassword) {
-  const maxIndex = char1.length;
+function bruteForceAttack09(targetPassword) {
+  const maxIndex = char2.length;
   let currentPassword = "";
   const startTime = new Date();
 
   function generateNextPassword(password) {
     if (password === "") {
-      return char1[0];
+      return char2[0];
     }
 
     let i = password.length - 1;
     let carry = true;
 
     while (i >= 0 && carry) {
-      const charIndex = char1.indexOf(password[i]);
+      const charIndex = char2.indexOf(password[i]);
       if (charIndex < maxIndex - 1) {
         password =
           password.substring(0, i) +
-          char1[charIndex + 1] +
+          char2[charIndex + 1] +
           password.substring(i + 1);
         carry = false;
       } else {
         password =
-          password.substring(0, i) + char1[0] + password.substring(i + 1);
+          password.substring(0, i) + char2[0] + password.substring(i + 1);
         i--;
       }
     }
 
     if (carry) {
-      password = char1[0] + password;
+      password = char2[0] + password;
     }
 
     return password;
@@ -58,13 +58,13 @@ function bruteForceAttack01(targetPassword) {
   };
 }
 
-function startBruteForceAttack01() {
-  const targetPassword = document.getElementById("passwordInput1").value;
+function startBruteForceAttack09() {
+  const targetPassword = document.getElementById("passwordInput2").value;
 
   if (targetPassword === "") {
     alert("Veuillez entrer un mot de passe.");
     return;
   }
 
-  bruteForceAttack01(targetPassword);
+  bruteForceAttack09(targetPassword);
 }
